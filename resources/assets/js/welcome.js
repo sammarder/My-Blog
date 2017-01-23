970444792811cbfc6ea56e5914062dea5f3ee025
Sam Marder <smardercollege@gmail.com>
2017-01-23 11:02:29 -0500

    Adding the ability to track key strokes

--- resources/assets/js/welcome.js
+++ resources/assets/js/welcome.js
@@ -1,3 +1,5 @@
+var countTrack = 0;
+var countImage = 0;
 function foo(name) {
     alert("hey " + name);
 }
@@ -9,10 +11,28 @@ function hide (elem) {
     elem.style.display=""; 
 }
 
+/*
+So the count variable is constant within a session
+Next step: Figure out how to get max value of array in here
+After that, figure out how to adjust the image and song accordingly
+*/
 function myF(event) {
-    var x = event.key;
+    var keyPressed = event.key;
     var expr = new RegExp("^Arrow.*");
-    if (expr.test(x)) {
+    if (keyPressed === "ArrowLeft" || keyPressed === "ArrowDown") {
+        countTrack--;
+    }
+    else if (keyPressed === "ArrowRight" || keyPressed === "ArrowUp") {
+        countTrack++;
+    }
+    else if (keyPressed === "a" || keyPressed === "s") {
+        countImage--;
+    }
+    else if (keyPressed === "d" || keyPressed === "w") {
+        countImage++;
+    }
+    //alert("You have gone to image " + countImage + " and song " + countTrack + ".");
+    if (!expr.match(x)) {
         var newTrack = "http://mah-pi/music/07 - Aurora.mp3"
         var audioElement = document.getElementById("song");
         if (audioElement) {
