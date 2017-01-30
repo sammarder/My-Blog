7e574bcbab6974036423f5e9fecf9da75c3d5aa7
Sam Marder <smardercollege@gmail.com>
2017-01-30 13:26:59 -0500

    Making the information a little mroe useful to the user

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -19,7 +19,7 @@
 
         <p class="left-align">This site makes use of the Laravel PHP framework as its back bone for routing and controlling the passage of information from the MySQL database to the frontend. Each page makes use of a different JS framework except for this one.</p>
 
-        <p class="left-align">The home page uses vanilla JS with events being managed by the HTML side hence hitting tab before being able to interact with it. It is encouraged to try the wasd and arrow keys here to see how the page reacts. Also, if you provide /?name=sam at the end of the home page, you can see various links to other parts of the website.</p>
+        <p class="left-align">The home page uses vanilla JS with events being managed by the HTML side hence hitting tab before being able to interact with it. It is encouraged to try the wasd and arrow keys here to see how the page reacts. Also, if you provide /?name=[name] at the end of the home page, you can see that name as the title of the page.</p>
 
         <p class="left-align">The photo page makes use of jQuery for interaction. All you have to do is hit wasd or an arrow key and the image will change along with the information about that image.</p>
     </body>
