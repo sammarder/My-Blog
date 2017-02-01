4fbe59c60eff8c44c52d3557036216b29e845103
Sam Marder <smardercollege@gmail.com>
2017-02-01 13:05:05 -0500

    Fixing the wording on the about page of the website

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,9 +17,9 @@
     <body>
         <p class="left-align">I am a recent college grad playing around with the LAMP stack. I started this project after I got laid off from my first job out of college which left me hungry for more full stack development. This project was principally for me to understand what goes into real web design and to better understand different JS libraries. I have attempted to design each page as well as I could and in the process learning about CSS as much as possible.</p>
 
-        <p class="left-align">This site makes use of the Laravel PHP framework as its back bone for routing and controlling the passage of information from the MySQL database to the frontend. Each page makes use of a different JS framework except for this one.</p>
+        <p class="left-align">This site makes use of the Laravel PHP framework as its back bone for routing and controlling the passage of information from the MySQL database to the frontend. Each page makes use of a different JS library except for this one.</p>
 
-        <p class="left-align">The home page uses vanilla JS with events being managed by the HTML side hence hitting tab before being able to interact with it. It is encouraged to try the wasd and arrow keys here to see how the page reacts. Also, if you provide /?name=[name] at the end of the home page, you can see that name as the title of the page.</p>
+        <p class="left-align">The home page uses vanilla JS with events being managed by the HTML side. It is encouraged to try hitting tab and either the wasd or arrow keys here to see how the page reacts. Also, if you provide /?name=[name] at the end of the home page, you can see that name as the title of the page.</p>
 
         <p class="left-align">The photo page makes use of jQuery for interaction. All you have to do is hit wasd or an arrow key and the image will change along with the information about that image.</p>
     </body>
