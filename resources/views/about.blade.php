79473ffeb1aed65d9bf93197bd990ca5f334aa5f
Sam Marder <smardercollege@gmail.com>
2017-02-07 11:45:42 -0500

    Rewording a bit of it

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,7 +15,7 @@
         <script type="text/javascript" src="{{ URL::asset('js/photo.js') }}"></script>
     </head>
     <body>
-        <p class="left-align">I am a recent college grad playing around with the LAMP stack. I started this project after I got laid off from my first job out of college which left me hungry for more full stack development. This project was principally for me to understand what goes into real web design and to better understand different JS libraries. I have attempted to design each page as well as I could and in the process learning about CSS as much as possible.</p>
+        <p class="left-align">I am a recent college grad playing around with the LAMP stack. My first job out of college inspired me to pursue full stack development. This project was principally for me to understand what goes into real web design and to better understand different JS libraries. I have attempted to design each page as well as I could and in the process learning about CSS as much as possible.</p>
 
         <p class="left-align">This site makes use of the Laravel PHP framework as its back bone for routing and controlling the passage of information from the MySQL database to the frontend. Each page makes use of a different JS library except for this one.</p>
 
