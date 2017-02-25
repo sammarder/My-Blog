abf201ee83c38950693f5e17f3378a8e42e96c64
Sam Marder <smardercollege@gmail.com>
2017-02-24 22:03:28 -0500

    Removing autoplay from blade

--- resources/views/welcome/music.blade.php
+++ resources/views/welcome/music.blade.php
@@ -1,4 +1,4 @@
 <p>{{$track->title}} by {{$track->artist}}</p>
-<audio id="myMusic" onended="next(event, '{{$name}}', {{$trackNum}})" controls {{$auto}} title="Change by hitting tab followed by the arrow keys">
+<audio id="myMusic" onended="next(event, '{{$name}}', {{$trackNum}})" controls title="Change by hitting tab followed by the arrow keys">
     <source id="song" src="{{ URL::asset('music/'.$track->filename) }}" type="audio/mpeg">
 </audio>
