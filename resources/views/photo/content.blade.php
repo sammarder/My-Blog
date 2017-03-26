16a856f8d2a89eff77f2c3f5926ff2eb860920e4
Sam Marder <smardercollege@gmail.com>
2017-03-26 15:37:35 -0400

    Redoing what is displayed on the photo page

--- resources/views/photo/content.blade.php
+++ resources/views/photo/content.blade.php
@@ -3,18 +3,15 @@
         <button class="mobile" name="left"><</button>
     </div>
     <div class="column">
-        <img src="{{ URL::asset('img/'.$photo->name.'.jpg') }}" title="Try hitting wasd or arrow keys">
+        <img src="{{ URL::asset('img/photos/'.$photo->name.'.jpg') }}" title="Try hitting wasd or arrow keys">
     </div>
     <div class="column">
         <p class="left-center">
-            Image Title: {{ explode(".", $name)[0] }}<br><br>
-            @if ($photo)
-                <input type="hidden" name="clip" value="{{ $photo->clip }}">
-                Camera Model: {{ $photo->model }}<br>
-                Lens Length: {{ $photo->lens_length }}mm<br><br>
-                F Number: f/{{ $photo->f_number }}<br>
-                ISO: {{ $photo->iso }}<br>
-                Shutter Speed: {{ $photo->shutter_speed }} seconds
+            Image Title: {{ $photo->name }}<br><br>
+            <input type="hidden" name="clip" value="{{ $photo->clip }}">
+            Season: {{ $photo->season." ".$photo->year }} <br>
+            @if ($photo->location)
+                Location: {{ $photo->location }}<br>
             @endif
         </p>
     </div>
