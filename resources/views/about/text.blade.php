756c702bbb84ef93cf9637cfcc5564fb04bddc1a
Sam Marder <smardercollege@gmail.com>
2017-03-27 03:27:25 -0400

    Updating the about page to include information about the clips

--- resources/views/about/text.blade.php
+++ resources/views/about/text.blade.php
@@ -2,4 +2,4 @@
 
 <p class="left-align">This site makes use of the Laravel PHP framework as its back bone for routing and controlling the passage of information from the MySQL database to the frontend. Each page makes use of a different JS library except for this one.</p>
 
-<p class="left-align">How it works:<br>On the homepage, hit tab folled by either the wasd or the arrow keys<br>On the photo page, try just hitting wasd or arrow keys<br>Sadly this does not work on all browsers at this time</p>
+<p class="left-align">How it works:<br>On the homepage, hit tab folled by either the wasd or the arrow keys<br>On the photo page, try just hitting wasd or arrow keys. Some of the nature shots will play a sound when clicked.<br>Sadly this does not work on all browsers at this time</p>
