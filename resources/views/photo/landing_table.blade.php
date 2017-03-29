3506743f31f37af4de9eae9de180cd9cf5a89b0e
Sam Marder <smardercollege@gmail.com>
2017-03-29 07:17:10 -0400

    Adding the generated urls to the blade

--- resources/views/photo/landing_table.blade.php
+++ resources/views/photo/landing_table.blade.php
@@ -4,7 +4,9 @@
             <tr>
                 @foreach ($row as $pic)
                     <td>
-                        <img src="{{ URL::asset('img/photos/thumbnails/'.$pic->name.'.jpg') }}">
+                        <a href="{{route('detail', ['season' => 'test', 'id' => $pic->index])}}">
+                            <img src="{{ URL::asset('img/photos/thumbnails/'.$pic->name.'.jpg') }}">
+                        </a>
                     </td>
                 @endforeach
             </tr>
