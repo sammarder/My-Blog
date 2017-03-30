8040d554141b2a1987d961d184e120dbee5f24b4
Sam Marder <smardercollege@gmail.com>
2017-03-29 22:51:38 -0400

    Adding a post button to have something to attach event to

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,12 +1,20 @@
 <html lang="en">
     @include("photo.head")
     <body>
-        <select>
-            <option value="all">All</option>
-            @foreach($seasons as $season)
-                <option value="{{ $season["value"] }}">{{ $season["display"] }}</option>
-            @endforeach
-        </select>
+        <form id="landing" method="POST">
+            {{ csrf_field() }}
+            <div class="content">
+                <div class="row">
+                    <select>
+                        <option value="all">All</option>
+                        @foreach($seasons as $season)
+                            <option value="{{ $season["value"] }}">{{ $season["display"] }}</option>
+                        @endforeach
+                    </select>
+                    <button type="submit">Search</button>
+                </div>
+            </div>
         @include("photo.landing_table")
+        </form>
     </body>
 </html>
