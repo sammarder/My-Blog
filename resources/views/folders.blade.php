8ab593e2de939b5285f68457edb8ae2e14c27766
Sam Marder <smardercollege@gmail.com>
2017-04-01 03:48:15 -0400

    Adding look and feel to the folder page

--- resources/views/folders.blade.php
+++ resources/views/folders.blade.php
@@ -1,16 +1,21 @@
+<!DOCTYPE html>
 <html lang="en">
     @include("photo.head")
     <body>
         <div class="center.me">
             <div class="content">
+                <div class="column">
                 <div class="row">
                     <div class="title m-b-md">
                         Photography
                     </div>
                 </div>
                 <div class="row">
-                    @include("photo.folders")
+                    <div class="center-me">
+                        @include("photo.folders")
+                    </div>
                 </div>
+             </div>
             </div>
         </div>
      </body>
--- resources/views/photo/folders.blade.php
+++ resources/views/photo/folders.blade.php
@@ -3,7 +3,14 @@
         <tr>
             @foreach ($row as $season)
                 <td>
-                    <a href="{{ route('thumbnails', ['season' => $season['value']]) }}">{{ $season['display'] }}</a>
+                    <a href="{{ route('thumbnails', ['season' => $season['value']]) }}">
+                        <div class="row">
+                            <img class="folder" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Folder-orange.svg/1024px-Folder-orange.svg.png">
+                        </div>
+                        <div class="row">
+                            {{ $season['display'] }}
+                        </div>
+                    </a>
                 </td>
             @endforeach
         </tr>
